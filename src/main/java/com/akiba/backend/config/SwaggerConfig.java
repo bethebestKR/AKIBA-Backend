f83d02069226481039149d78c067494a711c5f3c
bethebestKR <vtr1844@naver.com>
2026-05-13 23:09:08 +0900

    도메인 새로 산거로 수정

--- src/main/java/com/akiba/backend/config/SwaggerConfig.java
+++ src/main/java/com/akiba/backend/config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig {
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", securityScheme))
                 .addSecurityItem(securityRequirement)
-                .addServersItem(new Server().url("http://3.38.67.165:8081"))//Swagger가 API 호출할 때 URL을 절대경로말고 상대경로로 변경
+                .addServersItem(new Server().url("https://api.dev.akiba-shop.com"))
                 .addServersItem(new Server().url("http://localhost:8080"));
 
     }
